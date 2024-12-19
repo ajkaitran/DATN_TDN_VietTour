2c5fe9b05e7f8b9b8cdb485edfc5f7f1f78eac78
Duonglor <quyennguyen1130806@gmail.com>
2024-12-19 16:24:56 +0700

    feature tour

--- DATN/resources/views/tour/edit.blade.php
+++ DATN/resources/views/tour/edit.blade.php
@@ -1,3 +1,174 @@
-<div>
-    <!-- I begin to speak only when I am certain what I will say is not better left unsaid. - Cato the Younger -->
-</div>
+@extends('shared._layoutAdmin')
+@section('title', 'Sửa Tour')
+
+@section('content')
+    <h2 class="title_page">Sửa Tour</h2>
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    @if (session('success'))
+        <div class="alert alert-success">
+            {{ session('success') }}
+        </div>
+    @endif
+
+    @if (session('error'))
+        <div class="alert alert-danger">
+            {{ session('error') }}
+        </div>
+    @endif
+
+    <div class="box_content">
+        <a class="box_link" href="{{ route('tour.index') }}">Danh sách Tour</a>
+        <div class="content px-3">
+            <form action="{{ route('tour.update', $tour->id) }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                @method('PUT')
+                <div class="row mx-3">
+                    <div class="col-8">
+                        <div class="form-group">
+                            <label class="form_ext w-25" for="">Tên Tour</label>
+                            <input type="text" class="form-control w-75" name="name" value="{{ $tour->name }}">
+                        </div>
+                        <div class="form-group">
+                            <label class="form_ext w-25" for="">Địa chỉ Email</label>
+                            <input type="text" class="form-control w-75" name="email" value="{{ $tour->email }}">
+                        </div>
+                        <div class="form-group">
+                            <label for="" class="form_ext w-25">Mô tả</label>
+                            <textarea class="form-control w-75" cols="20" rows="4" name="description">
+                                {{ $tour->description }}
+                            </textarea>
+                        </div>
+                        <div class="form-group">
+                            <label class="form_ext w-25" for="">Danh Mục Tour</label>
+                            <div class=" tbody-item-flex w-75">
+                                <select class="form-control w-50" name="product_category_id" id="">
+                                    @foreach ($categoryTour as $category)
+                                        <option value="{{ $category->id }}"
+                                            {{ $category->id == $tour->product_category_id ? 'selected' : '' }}>
+                                            {{ $category->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="form_ext w-25" for="">Giá 1:</label>
+                            <input type="text" class="form-control w-75" name="price1" value="{{ $tour->price1 }}">
+                        </div>
+                        <div class="form-group">
+                            <label class="form_ext w-25" for="">Giá 2:</label>
+                            <input type="text" class="form-control w-75" name="price2" value="{{ $tour->price2 }}">
+                        </div>
+                        <div class="form-group">
+                            <label class="form_ext w-25" for="">Giá 3:</label>
+                            <input type="text" class="form-control w-75" name="price3" value="{{ $tour->price3 }}">
+                        </div>
+                        <div class="form-group">
+                            <label class="form_ext w-25" for="">Giá 4:</label>
+                            <input type="text" class="form-control w-75" name="price4" value="{{ $tour->price4 }}">
+                        </div>
+                        <div class="form-group">
+                            <label class="form_ext w-25" for="">Giá 5:</label>
+                            <input type="text" class="form-control w-75" name="price5" value="{{ $tour->price5 }}">
+                        </div>
+                        <div class="form-group">
+                            <label class="form_ext w-25" for="">Giá Trung Bình:</label>
+                            <input type="text" class="form-control w-75" name="original_price"
+                                value="{{ $tour->original_price }}">
+                        </div>
+                        <div class="form-group">
+                            <label class="form_ext w-25" for="">Đặt cọc:</label>
+                            <input type="text" class="form-control w-75" name="dat_coc" value="{{ $tour->dat_coc }}">
+                        </div>
+                        <div class="form-group">
+                            <label class="form_ext w-25" for="">Độ tuổi trẻ em:</label>
+                            <input type="text" class="form-control w-75" name="do_tuoi_tre_em"
+                                value="{{ $tour->do_tuoi_tre_em }}">
+                        </div>
+                        <div class="form-group">
+                            <label class="form_ext w-25" for="">Độ tuổi trẻ nhỏ:</label>
+                            <input type="text" class="form-control w-75" name="do_tuoi_tre_nho"
+                                value="{{ $tour->do_tuoi_tre_nho }}">
+                        </div>
+                        <div class="form-group">
+                            <label class="form_ext w-25" for="">Độ tuổi em bé:</label>
+                            <input type="text" class="form-control w-75" name="do_tuoi_em_be"
+                                value="{{ $tour->do_tuoi_em_be }}">
+                        </div>
+                        <div class="form-group">
+                            <label class="form_ext w-25" for="">Tiêu đề phụ thu:</label>
+                            <input type="text" class="form-control w-75" name="tieu_de_phu_thu"
+                                value="{{ $tour->tieu_de_phu_thu }}">
+                        </div>
+                        <div class="form-group">
+                            <label class="form_ext w-25" for="">Mã Combo:</label>
+                            <input type="text" class="form-control w-75" name="product_code"
+                                value="{{ $tour->product_code }}">
+                        </div>
+                        <div class="form-group">
+                            <label class="form_ext w-25" for="TimeDetail">Thời gian Tour:</label>
+                            <select class="form-control w-75" name="time" required>
+                                @for ($i = 1; $i < 20; $i++)
+                                    @if ($i == 1)
+                                        <option value="{{ $i }}" data-time="{{ $i }}"
+                                            {{ $tour->time == $i ? 'selected' : '' }}>
+                                            {{ $i }} ngày
+                                        </option>
+                                    @else
+                                        <option value="{{ $i }}" data-time="{{ $i }}"
+                                            {{ $tour->time == $i ? 'selected' : '' }}>
+                                            {{ $i }} ngày {{ $i - 1 }} đêm
+                                        </option>
+                                    @endif
+                                @endfor
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label class="form_ext w-25" for="">Quà:</label>
+                            <input type="text" class="form-control w-75" name="gift" value="{{ $tour->gift }}">
+                        </div>
+                        <div class="form-group">
+                            <label class="form_ext w-25" for="">Chi tiết quà:</label>
+                            <input type="text" class="form-control w-75" name="gift_note"
+                                value="{{ $tour->gift_note }}">
+                        </div>
+                        <div class="form-group">
+                            <label class="form_ext w-25" for="sort">Thứ Tự</label>
+                            <input type="number" class="form-control w-75" id="" name="slot"
+                                value="{{ $tour->slot }}">
+                        </div>
+                        <div class="form-group">
+                            <label class="form_ext w-25" for="slogan">Url</label>
+                            <input type="text" class="form-control w-75" id="" name="url"
+                                value="{{ $tour->url }}">
+                        </div>
+                        <div class="form-group">
+                            <label class="form_ext w-25" for="slogan">Url đặt tour:</label>
+                            <input type="text" class="form-control w-75" id="" name="url_book_tour"
+                                value="{{ $tour->url_book_tour }}">
+                        </div>
+                        <div class="form_check">
+                            <label class="form_ext w-25" for="active">Trạng Thái</label>
+                            <input type="checkbox" id="active" name="active" value="1"
+                                {{  $tour->active == 1 ? 'checked' : '' }}>
+                        </div>
+
+                        <button type="submit" class="btn btn-success">Cập nhật</button>
+                        <a href="{{ route('tour.index') }}" class="btn btn-secondary">Quay lại</a>
+                    </div>
+                </div>
+            </form>
+
+        </div>
+    </div>
+
+@endsection
--- DATN/resources/views/tour/index.blade.php
+++ DATN/resources/views/tour/index.blade.php
@@ -33,11 +33,9 @@
                 <thead class="thead">
                     <tr>
                         <th style="width: 50px;" scope="col">STT</th>
-                        <th style="width: 120px;" scope="col">Hình Ảnh</th>
                         <th style="width: 350px;" scope="col">Thông Tin Tour</th>
-                        <th style="width: 150px;" scope="col">Giá tiền</th>
+                        <th style="width: 250px;" scope="col">Giá tiền</th>
                         <th style="width: 200px;" scope="col">Danh Mục</th>
-                        <th style="width: 120px;" scope="col">Trạng Thái</th>
                         <th></th>
                     </tr>
                 </thead>
@@ -46,54 +44,43 @@
                         <tr>
                             <td>{{ $key + 1 }}</td>
                             <td>
-                                <img src="{{ Storage::url('tour/' . $tour->image) }}" style="width: 100%;">
-                            </td>
-                            <td>
-                                <div class="tbody-item-column">
-                                    <p>{{ $tour->name }}</p>
+                                <div class="tbody-item-column pl-5">
+                                    <p>Tên tour: {{ $tour->name }}</p>
                                     <div class="tbody-item-flex">
                                         <p>Mã SP: {{ $tour->product_code }}</p>
-                                        <p class="p_check">Nổi Bật: <input type="checkbox" name="is_hot"
-                                                data-id="{{ $tour->id }}" id=""
-                                                {{ $tour->is_hot == 1 ? 'checked' : '' }}>
-                                        </p>
                                         <p class="p_check">Trang Chủ: <input type="checkbox" name="active"
                                                 data-id="{{ $tour->id }}" id=""
                                                 {{ $tour->active == 1 ? 'checked' : '' }}>
                                         </p>
                                     </div>
                                     <div class="tbody-item-flex">
-                                        <a href="#">Nhân Đôi</a>
-                                        <a href="#">Xem Thử</a>
-                                    </div>
-                                    <div class="tbody-item-flex">
-                                        <a href="#">Danh Sách Đánh Giá</a>
-                                        <a href="#">Danh Sách Câu Hỏi</a>
+                                        <p>Email: {{ $tour->email }}</p>
                                     </div>
                                 </div>
                             </td>
                             <td>
-                                <div class="tbody-item-column">
-                                    <p>Giá KM:</p>
-                                    <input type="text" name="price" id="" value="{{ $tour->price }}">
-                                </div>
-                                <div class="tbody-item-column">
-                                    <p>Giá Bán:</p>
-                                    <input type="text" name="sale_off" value="{{ $tour->sale_off }}" id="">
-                                </div>
-                            </td>
-                            <td>
-                                <div class="tbody-item-column">
-                                    {{ $tour->category->name ?? 'Chua co danh muc' }}
+                                <div class="tbody-item-column pl-5">
+                                    <p>Giá 1: {{ $tour->price1 }}</p>
+                                    <p>Giá 2: {{ $tour->price2 }}</p>
+                                    <p>Giá 3: {{ $tour->price3 }}</p>
+                                    <p>Giá 4: {{ $tour->price4 }}</p>
+                                    <p>Giá 5: {{ $tour->price5 }}</p>
+                                    <p>Giá gốc: {{ $tour->original_price }}</p>
                                 </div>
                             </td>
                             <td>
-                                <input type="checkbox" name="active" data-id="{{ $tour->id }}" id="">
+                                <div class="tbody-item-column">
+                                    {{ $tour->product_category->name ?? 'Chua co danh muc' }}
+                                </div>
                             </td>
-                            <td>
-                                <a class="btn btn-primary mb-2" href="#">Cập nhật</a> <br>
-                                <a class="btn btn-warning mr-1" href="#">Sửa</a>
-                                <a class="btn btn-danger ml-1" href="#">Xóa</a>
+                            <td class="d-flex justify-content-center align-center pt-5 gap-3">
+                                <a class="btn btn-warning mr-1"
+                                    href="{{ route('tour.edit', ['id' => $tour->id]) }}">Sửa</a>
+                                <form action="{{ route('tour.destroy', ['id' => $tour->id]) }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="btn btn-danger">Xóa</button>
+                                </form>
                             </td>
                         </tr>
                     @endforeach
