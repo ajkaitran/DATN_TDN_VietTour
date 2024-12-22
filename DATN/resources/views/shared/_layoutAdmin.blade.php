7bd2c9b37c3cbb214a7c5762f5241af83bcd06be
Duonglor <quyennguyen1130806@gmail.com>
2024-12-22 16:11:21 +0700

    feature category article

--- DATN/resources/views/shared/_layoutAdmin.blade.php
+++ DATN/resources/views/shared/_layoutAdmin.blade.php
@@ -11,11 +11,8 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
         integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/jquery-toast-plugin/dist/jquery.toast.min.css">
-    <link rel="stylesheet" href="{{url('font-awesome/css/all.css')}}">
-    @vite([
-        'resources/css/style_admin.scss',
-        'resources/css/template_admin.css',
-    ])
+    <link rel="stylesheet" href="{{ url('font-awesome/css/all.css') }}">
+    @vite(['resources/css/style_admin.scss', 'resources/css/template_admin.css'])
     <title>Admin</title>
 </head>
 
@@ -26,18 +23,21 @@
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center my-3" href="{{route('home.index')}}">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center my-3"
+                href="{{ route('home.index') }}">
                 <div class="sidebar-brand-icon d-lg-none">
-                    <img src="{{url('images/Logo_VietTour (2).png')}}" style="width: 100%; height: 100px; object-fit: cover;" alt="">
+                    <img src="{{ url('images/Logo_VietTour (2).png') }}"
+                        style="width: 100%; height: 100px; object-fit: cover;" alt="">
                 </div>
-                <div class="sidebar-brand-text mx-3"><img src="{{url('images/Logo_VietTour (1).png')}}" style="width: 100%; object-fit: cover; padding: 15px;" alt="ảnh logo"></div>
+                <div class="sidebar-brand-text mx-3"><img src="{{ url('images/Logo_VietTour (1).png') }}"
+                        style="width: 100%; object-fit: cover; padding: 15px;" alt="ảnh logo"></div>
             </a>
             <!-- Divider -->
             <hr class="sidebar-divider my-0">
 
             <!-- Nav Item - Dashboard -->
             <li class="nav-item active">
-                <a class="nav-link" href="{{route('admin.index')}}">
+                <a class="nav-link" href="{{ route('admin.index') }}">
                     <i class="fa-light fa-house"></i>
                     <span>TỔNG QUAN</span>
                 </a>
@@ -62,9 +62,10 @@
                     <div class="bg-white py-2 collapse-inner rounded">
                         <h6 class="collapse-header">Custom Components:</h6>
                         <!-- <a class="collapse-item" href="?controller=Admin&action=UpdatePassword">Đổi mật khẩu</a> -->
-                        <a class="collapse-item" href="{{route('admin.register')}}">Quản lý admin</a>
+                        <a class="collapse-item" href="{{ route('admin.register') }}">Quản lý admin</a>
                         <a class="collapse-item" href="#">Thông tin chung</a>
-                        <a class="collapse-item" href="{{ route('admin.signout') }}" onclick="return confirm('Bạn có chắc chắn muốn đăng xuất?');">
+                        <a class="collapse-item" href="{{ route('admin.signout') }}"
+                            onclick="return confirm('Bạn có chắc chắn muốn đăng xuất?');">
                             Đăng xuất
                         </a>
                     </div>
@@ -107,11 +108,11 @@
                 <div id="collapsePages1" class="collapse" aria-labelledby="headingPages"
                     data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <a class="collapse-item" href="{{route('tourType.index')}}">Loại Tour</a>
-                        <a class="collapse-item" href="{{route('tourCategory.index')}}">Danh mục Tour</a>
-                        <a class="collapse-item" href="{{route('tour.index')}}">Danh sách Tour</a>
-                        <a class="collapse-item" href="{{route('combo.index')}}">Danh sách Combo</a>
-                        <a class="collapse-item" href="{{route('startPlace.index')}}">Điểm xuất phát</a>
+                        <a class="collapse-item" href="{{ route('tourType.index') }}">Loại Tour</a>
+                        <a class="collapse-item" href="{{ route('tourCategory.index') }}">Danh mục Tour</a>
+                        <a class="collapse-item" href="{{ route('tour.index') }}">Danh sách Tour</a>
+                        <a class="collapse-item" href="{{ route('combo.index') }}">Danh sách Combo</a>
+                        <a class="collapse-item" href="{{ route('startPlace.index') }}">Điểm xuất phát</a>
                     </div>
                 </div>
             </li>
@@ -125,8 +126,8 @@
                 <div id="collapsePages2" class="collapse" aria-labelledby="headingPages"
                     data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <a class="collapse-item" href="{{route('article.index')}}">Danh sách bài viết</a>
-                        <a class="collapse-item" href="{{route('article.create')}}">Thêm mới bài viết</a>
+                        <a class="collapse-item" href="{{ route('article.index') }}">Danh sách bài viết</a>
+                        <a class="collapse-item" href="{{ route('categoryArticle.index') }}">Danh mục bài viết</a>
                     </div>
                 </div>
             </li>
@@ -139,8 +140,8 @@
                 <div id="collapsePages3" class="collapse" aria-labelledby="headingPages"
                     data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <a class="collapse-item" href="{{route('feedback.index')}}">Danh sách phản hồi</a>
-                        <a class="collapse-item" href="{{route('feedback.create')}}">Thêm mới phản hồi</a>
+                        <a class="collapse-item" href="{{ route('feedback.index') }}">Danh sách phản hồi</a>
+                        <a class="collapse-item" href="{{ route('feedback.create') }}">Thêm mới phản hồi</a>
                     </div>
                 </div>
             </li>
@@ -154,13 +155,13 @@
                 <div id="collapsePages4" class="collapse" aria-labelledby="headingPages"
                     data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <a class="collapse-item" href="{{route('banner.index')}}">Danh sách quảng cáo</a>
-                        <a class="collapse-item" href="{{route('banner.create')}}">Thêm mới quảng cáo</a>
+                        <a class="collapse-item" href="{{ route('banner.index') }}">Danh sách quảng cáo</a>
+                        <a class="collapse-item" href="{{ route('banner.create') }}">Thêm mới quảng cáo</a>
                     </div>
                 </div>
             </li>
             <li class="nav-item">
-                <a class="nav-link collapsed" href="{{route('comment.index')}}">
+                <a class="nav-link collapsed" href="{{ route('comment.index') }}">
                     <i class="fas fa-fw fa-cog"></i>
                     <span>Quản lý bình luận</span>
                 </a>
@@ -242,7 +243,8 @@
                                     </div>
                                     <div>
                                         <div class="small text-gray-500">December 12, 2019</div>
-                                        <span class="font-weight-bold">A new monthly report is ready to download!</span>
+                                        <span class="font-weight-bold">A new monthly report is ready to
+                                            download!</span>
                                     </div>
                                 </a>
                                 <a class="dropdown-item d-flex align-items-center" href="#">
@@ -267,7 +269,8 @@
                                         Spending Alert: We've noticed unusually high spending for your account.
                                     </div>
                                 </a>
-                                <a class="dropdown-item text-center small text-gray-500" href="#">Show All Alerts</a>
+                                <a class="dropdown-item text-center small text-gray-500" href="#">Show All
+                                    Alerts</a>
                             </div>
                         </li>
 
@@ -287,8 +290,8 @@
                                 </h6>
                                 <a class="dropdown-item d-flex align-items-center" href="#">
                                     <div class="dropdown-list-image mr-3">
-                                        <img class="rounded-circle" src="https://source.unsplash.com/fn_BT9fwg_E/60x60"
-                                            alt="">
+                                        <img class="rounded-circle"
+                                            src="https://source.unsplash.com/fn_BT9fwg_E/60x60" alt="">
                                         <div class="status-indicator bg-success"></div>
                                     </div>
                                     <div class="font-weight-bold">
@@ -299,8 +302,8 @@
                                 </a>
                                 <a class="dropdown-item d-flex align-items-center" href="#">
                                     <div class="dropdown-list-image mr-3">
-                                        <img class="rounded-circle" src="https://source.unsplash.com/AU4VPcFN4LE/60x60"
-                                            alt="">
+                                        <img class="rounded-circle"
+                                            src="https://source.unsplash.com/AU4VPcFN4LE/60x60" alt="">
                                         <div class="status-indicator"></div>
                                     </div>
                                     <div>
@@ -311,20 +314,21 @@
                                 </a>
                                 <a class="dropdown-item d-flex align-items-center" href="#">
                                     <div class="dropdown-list-image mr-3">
-                                        <img class="rounded-circle" src="https://source.unsplash.com/CS2uCrpNzJY/60x60"
-                                            alt="">
+                                        <img class="rounded-circle"
+                                            src="https://source.unsplash.com/CS2uCrpNzJY/60x60" alt="">
                                         <div class="status-indicator bg-warning"></div>
                                     </div>
                                     <div>
-                                        <div class="text-truncate">Last month's report looks great, I am very happy with
+                                        <div class="text-truncate">Last month's report looks great, I am very happy
+                                            with
                                             the progress so far, keep up the good work!</div>
                                         <div class="small text-gray-500">Morgan Alvarez · 2d</div>
                                     </div>
                                 </a>
                                 <a class="dropdown-item d-flex align-items-center" href="#">
                                     <div class="dropdown-list-image mr-3">
-                                        <img class="rounded-circle" src="https://source.unsplash.com/Mv9hjnEUHR4/60x60"
-                                            alt="">
+                                        <img class="rounded-circle"
+                                            src="https://source.unsplash.com/Mv9hjnEUHR4/60x60" alt="">
                                         <div class="status-indicator bg-success"></div>
                                     </div>
                                     <div>
@@ -333,7 +337,8 @@
                                         <div class="small text-gray-500">Chicken the Dog · 2w</div>
                                     </div>
                                 </a>
-                                <a class="dropdown-item text-center small text-gray-500" href="#">Read More Messages</a>
+                                <a class="dropdown-item text-center small text-gray-500" href="#">Read More
+                                    Messages</a>
                             </div>
                         </li>
 
@@ -344,7 +349,7 @@
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">Valerie Luna</span>
-                                <img class="img-profile rounded-circle" src="{{url('images/avt.jpg')}}">
+                                <img class="img-profile rounded-circle" src="{{ url('images/avt.jpg') }}">
                             </a>
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
@@ -362,10 +367,11 @@
                                     Activity Log
                                 </a>
                                 <div class="dropdown-divider"></div>
-                                <form action="{{route('admin.signout')}}" method="POST">
+                                <form action="{{ route('admin.signout') }}" method="POST">
                                     @csrf
-                                    <button class="dropdown-item" type="submit" onclick="return confirm('Bạn có chắc chắn muốn đăng xuất?')">
-                                        <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i> 
+                                    <button class="dropdown-item" type="submit"
+                                        onclick="return confirm('Bạn có chắc chắn muốn đăng xuất?')">
+                                        <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                                         Logout
                                     </button>
                                 </form>
@@ -379,7 +385,7 @@
 
                 <!-- Begin Page Content -->
                 <div class="container-fluid">
-                    @yield("content")
+                    @yield('content')
                 </div>
                 <!-- /.container-fluid -->
 
@@ -408,10 +414,7 @@
     <script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/jquery.validate.min.js"></script>
     <script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/additional-methods.min.js"></script>
     <script src="https://cdn.ckeditor.com/4.21.0/standard/ckeditor.js"></script>
-    @vite([
-        'resources/js/app_admin.js',
-        'resources/js/template_admin.js'
-    ])
+    @vite(['resources/js/app_admin.js', 'resources/js/template_admin.js'])
 </body>
 
 </html>
