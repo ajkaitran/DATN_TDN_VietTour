0b0bfd90ee6cdb643af513e47c8f54ca992d0763
hoanph28422z <hoanndph28422@fpt.edu.vn>
2025-01-07 05:14:10 +0700

    Add: component SearchTour+ connect API search, Fix searchForm(useNavigate)

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,53 +1,26 @@
-// import './css/admin.css'
-// import './css/style_admin.scss'
-// import './css/style_main.scss'
-import './App.css'
-import { useRoutes } from 'react-router-dom';
-// import 'slick-carousel';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Home from './user/Home';
 import GetTourById from './user/GetTourByCate';
 import BookTour from './user/BookTour';
 import DetailTour from './user/DetailTour';
 import InternationalTour from './user/TourQuocTe';
-import '@fortawesome/fontawesome-free/css/all.min.css';
 import TourNoiDia from './user/TourNoiDia';
 import SearchTour from './user/SearchTour';
 
-const routeConfig = [
-  {
-    path: "/Order",
-    element: <BookTour/>
-  },
-  {
-    path:"/home",
-    element:< Home/>
-  },
-  {
-    path: "/tour",
-    element: < GetTourById />
-  },
-  {
-    path: "/search-tour",
-    element: <SearchTour/>
-  },
-  {
-    path: "/Tour-quoc-te",
-    element: <InternationalTour />
-  },
-  {
-    path: "/Tour-noi-dia",
-    element: <TourNoiDia />
-  },
-  {
-    path: "/Detail",
-    element: <DetailTour />
-  },
-  
-];
 function App() {
-  const routes = useRoutes(routeConfig);
-  
-  return <div>{routes}</div>
+  return (
+    <Routes>
+      <Route path="/Order" element={<BookTour />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/tour" element={<GetTourById />} />
+      <Route path="/search-tour" element={<SearchTour />} />
+      <Route path="/Tour-quoc-te" element={<InternationalTour />} />
+      <Route path="/Tour-noi-dia" element={<TourNoiDia />} />
+      <Route path="/Detail" element={<DetailTour />} />
+      <Route path="/" element={<DetailTour />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
--- FE/src/user/component/SearchForm.tsx
+++ FE/src/user/component/SearchForm.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const SearchForm = () => {
     const [keyword, setKeyword] = useState('');
+    const navigate = useNavigate();
 
-    const handleSearch = async (e:any) => {
+    const handleSearch = async (e: any) => {
         e.preventDefault();
         try {
             const response = await axios.get('http://127.0.0.1:8000/api/tour/search', {
@@ -13,6 +15,7 @@ const SearchForm = () => {
                 }
             });
             console.log(response.data); // Xử lý dữ liệu trả về từ API ở đây
+            navigate('/search-tour', { state: { data: response.data } }); // Chuyển tới trang SearchTour
         } catch (error) {
             console.error(error);
         }
