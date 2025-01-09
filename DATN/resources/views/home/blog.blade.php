e8e0b82cb56aa934833a642e27454e4cb7974a8c
MacBook pro <macbookpro@192.168.102.21>
2025-01-10 01:59:47 +0700

    blog

--- DATN/resources/views/home/blog.blade.php
+++ DATN/resources/views/home/blog.blade.php
@@ -5,6 +5,7 @@
 <div class="relative mb-5">
     <img src="{{ url('images/blog.jpg') }}">
 </div>
+
 <body class="bg-gray-100">
     <div class="container mx-auto p-4">
         <!-- Breadcrumb -->
@@ -27,43 +28,27 @@
                     <h2 class="text-blue-600 text-lg font-bold mb-4">
                         TÌM KIẾM
                     </h2>
-                    <input class="w-full p-2 border border-gray-300 rounded mb-4" placeholder="Nhập từ khóa tìm kiếm..." type="text" />
-                    <ul class="space-y-2">
-                        <li>
-                            <a class=" text-blue-500" href="#">
-                                Blog du lịch
-                            </a>
-                        </li>
-                        <li>
-                            <a class="text-blue-500" href="#">
-                                Review
-                            </a>
-                        </li>
-                        <li>
-                            <a class="text-blue-500" href="#">
-                                Điểm đến
-                            </a>
-                        </li>
-                        <li>
-                            <a class="text-blue-500" href="#">
-                                Ẩm thực
-                            </a>
-                        </li>
-                        <li>
-                            <a class="text-blue-500" href="#">
-                                Kinh nghiệm
-                            </a>
-                        </li>
-                    </ul>
-                    <button class="w-full bg-blue-500 text-white py-2 rounded mt-4">
-                        Tìm kiếm
-                    </button>
+                    <form action="{{ route('home.blog') }}" method="GET">
+                        <input class="w-full p-2 border border-gray-300 rounded mb-4" placeholder="Nhập từ khóa tìm kiếm..." type="text" name="search" value="{{ request('search') }}" />
+                        <ul class="space-y-2">
+                            @foreach($articleCategory as $category)
+                            <li>
+                                <a class=" text-blue-500" href="{{ route('home.blog', ['search' => $category->category_name]) }}">
+                                    {{$category->category_name}}
+                                </a>
+                            </li>
+                            @endforeach
+                        </ul>
+                        <button class="w-full bg-blue-500 text-white py-2 rounded mt-4">
+                            Tìm kiếm
+                        </button>
+                    </form>
                 </div>
             </div>
             <!-- Blog Content -->
             <div class="w-full lg:w-3/4">
                 <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                    @foreach($listArticles as $items)
+                    @foreach($blog as $items)
                     <div class="bg-white rounded-lg shadow overflow-hidden">
                         <img alt="A beautiful traditional Chinese building by a lake" class="w-full h-48 object-cover" height="400" src="{{ Storage::url('article/' . $items->image) }}" width="600" />
                         <div class="p-4">
@@ -78,11 +63,11 @@
                             </p>
                         </div>
                     </div>
-                    
+
                     @endforeach
                 </div>
                 <div class="my-3">
-                    {{$listArticles->links()}}  
+                    {{$blog->links()}}
                 </div>
             </div>
         </div>
