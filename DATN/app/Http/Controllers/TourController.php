a8ccb716caa325ca3f57fa538fd5d8a5dc23c616
ajkaitran <trannamhai7626@gmail.com>
2025-01-09 22:08:06 +0700

    FE_Log

--- DATN/app/Http/Controllers/TourController.php
+++ DATN/app/Http/Controllers/TourController.php
@@ -22,8 +22,10 @@ class TourController extends Controller
     {
         $tours = Product::whereNull('deleted_at')->paginate(5); // Lấy danh sách tour tour
         $ProductCategories = ProductCategory::all(); // Danh mục cha
-        // $childCategories = ProductCategory::whereNotNull('parent_id')->get(); // Danh mục con
-        return view('tour.index', compact('tours', 'ProductCategories'));
+        $parentCategories = ProductCategory::whereNull('parent_id')->get(); // Danh mục cha
+        $childCategories = ProductCategory::whereNotNull('parent_id')->get(); // Danh mục con
+        $tourtypes = ProductCategoryType::all();
+        return view('tour.index', compact('tours', 'ProductCategories', 'parentCategories', 'childCategories', 'tourtypes'));
     }
     public function create(StoreTourRequest $request)
     {
